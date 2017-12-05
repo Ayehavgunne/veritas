3a8dcc7a21d778100edb5e8d83dd20f9d2ddef84
Anthony <postanthony3000@gmail.com>
2017-12-05 09:59:29 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='tabularpy',
+	version='1.0',
+	description='Python tablular information manupulation library',
+	author='Anthony Post',
+	author_email='postanthony3000@gmail.com',
+	url='https://github.com/Ayehavgunne/tabularpy/',
+	py_modules=['tabularpy'],
+)
